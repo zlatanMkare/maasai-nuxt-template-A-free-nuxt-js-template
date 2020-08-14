06e831e5c8d14a2407cb8f04fe358bdeb413b031
job gathu <jobgathunjoroge@yahoo.com>
2020-08-14 16:11:16 +0200

    fix box shadow and background

--- components/blocks/Testimonials.vue
+++ components/blocks/Testimonials.vue
@@ -62,7 +62,7 @@ export default {
           border-radius: 5px;
           cursor: pointer;
           margin: 10px;
-          background: #f9fdf5;
+          background: #ffffff;
           box-shadow: 0 14px 28px rgba(0,0,0,0.05), 0 10px 10px rgba(0,0,0,0.12);
      }
 </style>
